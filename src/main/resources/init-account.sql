ee99d97e861350e129fc7c27f247f16115d86ea8
Sven Graupner <sgraupner@bht-berlin.de>
2026-06-17 21:17:28 +0200

    add src/main/resources/*.sql

---
+++ src/main/resources/init-account.sql
@@ -0,0 +1,7 @@
+-- grant user 'root' all privileges on all databases
+CREATE USER 'root'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+
+-- grant user 'freerider' all privileges on database FREERIDER_DB
+CREATE USER 'freerider'@'%' IDENTIFIED BY 'free.ride';
+GRANT ALL PRIVILEGES ON FREERIDER_DB.* to 'freerider'@'%';
